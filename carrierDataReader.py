241ab808dbc1d45225663c63e6f9bda20603c0d7
maerwan <hotwraith4803@gmail.com>
2025-04-01 16:07:06 +0200

    [NEW] log found events for comparison
    [NEW] log found events for comparison

--- carrierDataReader.py
+++ carrierDataReader.py
@@ -60,9 +60,10 @@ def Menu() :
                 carrierDB = json.load(open('testCallsigns.json', 'r'))
                 j = len(carrierDB['ID'])
                 for i in range(j):
+                    pos = []
                     ReadJournal('CarrierStats', i)
-                    ReadJournal('CarrierLocation', i)
-                    ReadJournal('CarrierJumpRequest', i)
+                    pos.append(ReadJournal('CarrierLocation', i))
+                    pos.append(ReadJournal('CarrierJumpRequest', i))                    
             except json.decoder.JSONDecodeError:
                 pass
         elif(selection == "2"):
@@ -157,11 +158,13 @@ def ReadJournal(keypass, z):
                     exit = False
                     break
                 if(data['CarrierID'] == carrierDB['logID'][z] and keypass == 'CarrierLocation'):
+                    position = (data['StarSystem'], data['timestamp'])
                     print(f"{carrierDB['ID'][z]} is in : {data['StarSystem']}")
                     print("This data was collected at: "+data["timestamp"]+"\n")
                     exit = False
                     break
                 if(data['CarrierID'] == carrierDB['logID'][z] and keypass == 'CarrierJumpRequest'):
+                    position = (data['SystemName'], data['timestamp'])
                     print(f"{carrierDB['ID'][z]} has requested a jump to : {data['SystemName']} ", end="")
                     try:
                         print(f"(body: {data['Body']})")
@@ -176,6 +179,10 @@ def ReadJournal(keypass, z):
     except(IndexError):
         print(f"No {keypass} data found for this carrier : {carrierDB['shortname'][z]}")
     except KeyError:
+        pass
+    try:
+        return position
+    except UnboundLocalError:
         pass
 
 def serviceCost(somme):
